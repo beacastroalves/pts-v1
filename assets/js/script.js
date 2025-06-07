49d41334ceb3f8638ae4c9f762a1b31b5cfee4d0
Beatriz Castro <beacastro1008@gmail.com>
2025-06-07 18:17:23 +0100

    ajustando sessões de preço e palestrantes

--- assets/js/script.js
+++ assets/js/script.js
@@ -121,6 +121,38 @@ document.addEventListener('DOMContentLoaded', function() {
   createDots(); // Cria os dots e define o slide inicial
   startAutoSlide(); // Inicia a rolagem automática quando o script é carregado
 
+  // const splide = new Splide('.splide', {
+  //   type: 'loop',
+  //   autoplay: true,
+  // });
+  // setTimeout(() => splide.mount());
+
+
+  const main = new Splide('#main-slider', {
+    type: 'fade',
+    pagination: false,
+    arrows: false,
+  });
+
+  const thumbnails = new Splide('#thumbnail-slider', {
+    rewind: true,
+    fixedWidth: 104,
+    fixedHeight: 104,
+    isNavigation: true,
+    gap: 10,
+    focus: 'center',
+    pagination: false,
+    cover: true,
+    dragMinThreshold: {
+      mouse: 4,
+      touch: 10,
+    },
+  });
+
+  main.sync(thumbnails);
+  main.mount();
+  thumbnails.mount();
+
   window.addEventListener('scroll', onScrollGlobal);
   onScrollGlobal();
 });
